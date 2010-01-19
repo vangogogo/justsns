4946f1a91fdaaa12267de77074ce3adf73fd6c01
huanghuibin <huanghuibin@cf87fd3a-03f0-11df-8c27-7d37615f5189>
2010-01-19 09:41:52 +0000

    nomsg

---
+++ trunk/protected/views/account/_top.php
@@ -0,0 +1,16 @@
+<div class=page_title> <!-- page_title begin -->
+	<h2><img src="../Public/images/applications.gif" />帐号管理</h2>
+	<div class="c"></div>
+</div><!-- page_title end -->
+<!-- 切换标签 begin  -->
+<div class="tab-menu">
+	<ul>
+		<li><a href="__URL__"><span>修改密码</span></a></li>
+		<li><a href="__URL__/account" class="on"><span>修改账号</span></a></li>
+		<li><a href="__URL__/security"><span>账号安全</span></a></li>
+		<li><a href="__URL__/score"><span>积分记录</span></a></li>
+		<li><a href="__URL__/score_faq"><span>积分说明</span></a></li>
+		<!--   <li><a href="__URL__/subscribe"><span>邮件提醒</span></a></li> -->
+	</ul>
+</div>
+<!-- 切换标签 end  -->
