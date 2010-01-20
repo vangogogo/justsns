241560e4e1044055d5d47e7a92daa78abaa20787
huanghuibin <huanghuibin@cf87fd3a-03f0-11df-8c27-7d37615f5189>
2010-01-20 09:32:53 +0000

    nomsg

--- trunk/protected/views/account/index.php
+++ trunk/protected/views/account/index.php
@@ -1,8 +1,98 @@
 <?php
     include('_top.php');
 ?>
-<div id="module_content">
-	<div id="module_content_title">
-		创建学习重点
-	</div>	
-</div>
+<?php
+		$CS=Yii::app()->jformvalidate;
+		echo $CS->beginForm(); 
+		$CS->setScenario($form->scenario);
+		$CS->setOptions(array(
+		
+			//'errorLabelContainer' 	=> 'div.container ul',
+			//'errorContainer'		=> 'div.container',
+			'errorElement'			=> "div",
+			//'wrapper' 				=> 'li',
+			'errorClass' 			=> 'invalid',
+			
+			//错误提示
+			'errorPlacement' => 'function(error, element) {element.parents("li").find("div.error_info").show(); var error_label = element.parent("div").next("div").find(".clue p");error.appendTo(error_label);}',
+			//正确显示
+			'success'=>'function(label) { label.parents("div.error_info").hide().prev(".success").show();}',
+			'keyup' 				=> false,
+			'focusout' 				=> true,
+			
+			'highlight' 			=> '
+				function(element,errorClass){
+
+			       		$(element).addClass("invalid");
+			       		$(element).parents("li").find("div.error_info").show().prev(".success").hide();					
+			}',
+			'submitHandler' => 'function(form){$.fn.EJFValidate.submitHandler(form);}'
+		));
+		
+?>
+<div class="data jsvform" style="padding-top:30px;"><!-- 修改密码 begin  -->
+<?php echo CHtml::errorSummary($form);?>
+	<ul>
+		<li>
+			<div class="cl">当前密码：<em>*</em></div>
+			<div class="cc">
+				<?php echo $CS->activePasswordField($form,'oldpassword',array('class'=>'t_input')); ?>
+			</div>
+			<div class="cr">
+				<div class="success hidden">
+					<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/fzcg_dh[1].gif" /></span>
+				</div>
+				<div class="error_info" style="position: relative;">
+					<div>
+						<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/th_ju[1].gif" /></span>
+						<div class="clue"><p class="error_content"></p><span class="clue_btm"></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="c"></div>
+		</li>
+		<li>
+			<div class="cl">新密码：<em>*</em></div>
+			<div class="cc">
+				<?php echo $CS->activePasswordField($form,'password',array('class'=>'t_input')); ?>
+			</div>
+			<div class="cr">
+				<div class="success hidden">
+					<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/fzcg_dh[1].gif" /></span>
+				</div>
+				<div class="error_info" style="position: relative;">
+					<div>
+						<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/th_ju[1].gif" /></span>
+						<div class="clue"><p class="error_content"></p><span class="clue_btm"></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="c"></div>
+		</li>
+		<li>
+			<div class="cl">再输入一遍密码：<em>*</em></div>
+			<div class="cc">
+				<?php echo $CS->activePasswordField($form,'repassword',array('class'=>'t_input')); ?>
+			</div>
+			<div class="cr">
+				<div class="success hidden">
+					<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/fzcg_dh[1].gif" /></span>
+				</div>
+				<div class="error_info" style="position: relative;">
+					<div>
+						<span><img src="<?php echo Yii::app()->theme->baseUrl ?>/public/images/th_ju[1].gif" /></span>
+						<div class="clue"><p class="error_content"></p><span class="clue_btm"></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="c"></div>
+		</li>
+		
+		<li><div class="cl"><em>&nbsp;</em></div><div class="cc">
+			<?php echo CHtml::submitButton('确认修改',array('class'=>'btn_b')); ?>
+			</div><div class="cr"></div>
+			<div class="c"></div>
+		</li>
+	</ul>
+</div><!-- 修改密码 end  -->
+<?php echo $CS->endForm(); ?>
